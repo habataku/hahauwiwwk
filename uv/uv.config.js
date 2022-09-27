1d62093c2aa5408d813b90db65bed953a283c582
Villager A <100194237+habataku@users.noreply.github.com>
2022-09-27 20:17:37 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://moonrising.me/bare/',
+    bare: 'https://porepore.up.railway.app/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
