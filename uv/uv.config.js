0d7b8ca62fa6bfca8af77a682cbea88d7cf932a8
Villager A <100194237+habataku@users.noreply.github.com>
2022-10-02 20:31:11 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://porepore.up.railway.app/bare/',
+    bare: 'https://uv.ycrgv.org/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
