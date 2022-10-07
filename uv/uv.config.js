d1bb7d5f2e2e8b6e389da81e3a964891cc234dbe
Villager A <100194237+habataku@users.noreply.github.com>
2022-10-07 11:52:43 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.ycrgv.org/',
+    bare: 'https://successful-headscarf-clam.cyclic.app/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
