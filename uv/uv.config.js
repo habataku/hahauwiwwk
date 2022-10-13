afe4edab071284eebc6d10ebb8d2fc240a28adeb
Villager A <100194237+habataku@users.noreply.github.com>
2022-10-13 22:51:33 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://kkik-heieirk.koyeb.app/bare/',
+    bare: 'https://uv.cherrybar.org/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
