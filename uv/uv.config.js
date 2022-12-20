06d01927e1e63efb1a126193ce43334829b12ca3
Villager A <100194237+habataku@users.noreply.github.com>
2022-12-20 22:55:49 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.chaoshistory.info/',
+    bare: 'https://uv.limitalgebra.net/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
